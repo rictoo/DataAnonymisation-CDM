18a7ed0af1ec9367789ccd5d0f836713ea25c3d0
mehak_g@outlook.com <mehak_g@outlook.com>
2022-12-14 16:07:03 +0000

    added file

---
+++ decrypt.py
@@ -0,0 +1,17 @@
+# To decrypt and save as plaintext file; specify file to decrypt, decrypted file destination, and key location
+def decrypt(to_decrypt, file_destination, key_location):
+    with open(key_location, 'rb') as f:
+        key = f.read()
+        
+    fernet = Fernet(key)
+
+    with open(to_decrypt, 'rb') as f:
+        encrypted = f.read()
+
+    decrypted = fernet.decrypt(encrypted)
+    with open(file_destination, 'wb') as f:
+        f.write(decrypted)
+
+
+# Decrypt file; replace encrypted_filename.csv by filename
+decrypt(encrypted_filename.csv, "decrypted_dataset.csv", "key.key")
